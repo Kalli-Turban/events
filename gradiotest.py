4cd65ac2842a6788b8c4df6ff18cc7732de3da29
Kalli-Turban <Kh.turban@turbix.de>
2025-08-07 20:53:45 +0200

    blafasel

--- gradiotest.py
+++ gradiotest.py
@@ -9,4 +9,5 @@ app = gr.Interface(fn=ping, inputs=[], outputs="text")
 print("✅ Kalli: Starte Gradio auf PORT", os.environ.get("PORT", 7860))
 
 # app.launch(server_port=int(os.environ.get("PORT", 7860)), share=False, debug=True)
-demo.queue().launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
+# demo.queue().launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
+demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
