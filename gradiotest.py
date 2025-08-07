e23b65a30943113bde853ca93e776d01a42c75b2
Kalli-Turban <Kh.turban@turbix.de>
2025-08-07 20:58:28 +0200

    aber jetzt

--- gradiotest.py
+++ gradiotest.py
@@ -4,10 +4,8 @@ import os
 def ping():
     return "✅ Render lebt!"
 
-app = gr.Interface(fn=ping, inputs=[], outputs="text")
+demo = gr.Interface(fn=ping, inputs=[], outputs="text")
 
 print("✅ Kalli: Starte Gradio auf PORT", os.environ.get("PORT", 7860))
 
-# app.launch(server_port=int(os.environ.get("PORT", 7860)), share=False, debug=True)
-# demo.queue().launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
 demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
