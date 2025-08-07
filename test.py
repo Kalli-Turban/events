045fe188b37645ca9b3d8a3a8f267378ad1bdd19
Kalli-Turban <Kh.turban@turbix.de>
2025-08-07 20:12:46 +0200

    2ter anlauf

--- test.py
+++ test.py
@@ -5,5 +5,6 @@ with gr.Blocks() as demo:
     gr.Markdown("✅ Gradio funktioniert!")
 
 print("✅ Testscript erreicht .launch()")
+demo.launch(server_port=int(os.environ.get("PORT", 7860)), debug=True)
 
-demo.launch(server_port=int(os.environ.get("PORT", 7860)))
+# demo.launch(server_port=int(os.environ.get("PORT", 7860)))
