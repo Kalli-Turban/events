41a27954aff4305e23147932cc8e508b5b7c9fd2
Kalli-Turban <Kh.turban@turbix.de>
2025-08-15 21:33:36 +0200

    mot Port

--- events_app.py
+++ events_app.py
@@ -352,5 +352,5 @@ with gr.Blocks(css=CUSTOM_CSS, title=f"{APP_TITLE} · {__APP_VERSION__}") as dem
     demo.load(fn=init_tipp, outputs=[tipp_md, tipp_btn], queue=False)
 
 if __name__ == "__main__":
-   #demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
-    demo.launch()
+    demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
+   #demo.launch()
